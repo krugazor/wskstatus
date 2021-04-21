0071564b66f150638e3803a43cb00ca0c54a464c
Zino <zino@poulet.org>
2021-04-21 12:13:36 +0200

    More or less working ADDED: various on-screen information TODO: fix the max height for dashboard-like usage

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
         .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser", from: "0.3.0"),
         .package(name: "AnyCodable", url: "https://github.com/Flight-School/AnyCodable", from: "0.4.0"),
         .package(name: "Kitura", url: "https://github.com/Kitura/Kitura", from: "2.9.1"),
-        .package(name: "KituraStencil", url: "https://github.com/Kitura/Kitura-StencilTemplateEngine", from: "1.11.1")
+        .package(name: "KituraStencil", url: "https://github.com/Kitura/Kitura-StencilTemplateEngine", from: "1.11.1"),
+        .package(name: "MarkdownKit", url:"https://github.com/objecthub/swift-markdownkit", from:"1.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -26,7 +27,8 @@ let package = Package(
                 .product(name: "TermPlot", package: "TermPlot"),
                 .product(name: "AnyCodable", package: "AnyCodable"),
                 .product(name: "Kitura", package: "Kitura"),
-                .product(name: "KituraStencil", package: "KituraStencil")
+                .product(name: "KituraStencil", package: "KituraStencil"),
+                .product(name: "MarkdownKit", package: "MarkdownKit"),
             ]),
         .testTarget(
             name: "wskstatusTests",
--- Sources/wskstatus/Kitura.swift
+++ Sources/wskstatus/Kitura.swift
@@ -1,6 +1,7 @@
 import Foundation
 import Kitura
 import KituraStencil
+import MarkdownKit
 
 extension Router {
     func webStart(_ port: Int = 8085) {
@@ -13,6 +14,7 @@ extension Router {
 }
 
 var data = ActivationStore(base: "", auth: "", namespace: "")
+var lastUpdate = Date()
 
 func setupWebServices(apibase: String, apiauth: String, apins: String, frame: TimeFrame) -> Router {
     data = ActivationStore(base: apibase, auth: apiauth, namespace: apins)
@@ -34,31 +36,13 @@ func setupWebServices(apibase: String, apiauth: String, apins: String, frame: Ti
     }
     data.timeframe = frame
     data.refresh {
-    }
-    let _ = Timer.scheduledTimer(withTimeInterval: 10, repeats: true) { timer in
-        // update the data
-        switch frame {
-        case .minutely:
-            data.minDate = Date(timeIntervalSinceNow: -total*60)
-        case .hourly:
-            data.minDate = Date(timeIntervalSinceNow: -total*3600)
-        case .daily:
-            data.minDate = Date(timeIntervalSinceNow: -total*24*3600)
-        case .weekly:
-            data.minDate = Date(timeIntervalSinceNow: -total*7*24*3600)
-        case .monthly:
-            data.minDate = Date(timeIntervalSinceNow: -total*30*24*3600) // yea yea, I know
-        case .yearly:
-            data.minDate = Date(timeIntervalSinceNow: -total*365*24*3600) // yea yea, I know
-        }
-        data.refresh {
-        }
+        lastUpdate = Date()
     }
     let router = Router()
     
     router.all(middleware: BodyParser(), StaticFileServer(path: "./Public"))
     router.add(templateEngine: StencilTemplateEngine())
-    
+        
     router.get("/") { req, res, next in
         try res.redirect("index.html")
         next()
@@ -72,7 +56,7 @@ func setupWebServices(apibase: String, apiauth: String, apins: String, frame: Ti
             deltas.append(i-activations.count)
         }
         
-        let computed : [[String:Codable]] = [
+        let computedgraphs : [[String:Codable]] = [
             ["x": deltas,
             "y": activations,
             "type": "line",
@@ -87,6 +71,51 @@ func setupWebServices(apibase: String, apiauth: String, apins: String, frame: Ti
             "xaxis": "x2",
             "yaxis": "y2"]
         ]
+        
+        let rankedMdw = data.top5.reduce("") { prev, cur in
+            return prev + "- **\(cur.name)**: \(cur.occurences) activations (\(cur.average)ms avg)\n"
+        }
+        let ranked = MarkdownParser.standard.parse(rankedMdw)
+        
+        let logs = data.last(10).reversed().compactMap( { activation -> String? in
+            activation.logs?.map({ log -> String in
+                var heading = activation.activationId+" \(activation.name): "
+                if activation.statusCode != 0 {
+                    heading = "<span style='color:red'><b>\(heading)</b></span>"
+                } else {
+                    heading = "<span><b>\(heading)</b></span>"
+                }
+                return heading+"<pre style='display:inline'>\(log)</pre><br/>\n"
+            }).joined()
+        }).joined()
+        
+        let computed : [String:Any] = [
+            "graphs" : computedgraphs,
+            "ranked" : HtmlGenerator.standard.generate(doc: ranked),
+            "logs" : logs,
+            "frame": "All the data is aggregated in \(frame.rawValue) bins"
+        ]
+        
+        if abs(lastUpdate.timeIntervalSinceNow) > 10 {
+            // update the data
+            switch frame {
+            case .minutely:
+                data.minDate = Date(timeIntervalSinceNow: -total*60)
+            case .hourly:
+                data.minDate = Date(timeIntervalSinceNow: -total*3600)
+            case .daily:
+                data.minDate = Date(timeIntervalSinceNow: -total*24*3600)
+            case .weekly:
+                data.minDate = Date(timeIntervalSinceNow: -total*7*24*3600)
+            case .monthly:
+                data.minDate = Date(timeIntervalSinceNow: -total*30*24*3600) // yea yea, I know
+            case .yearly:
+                data.minDate = Date(timeIntervalSinceNow: -total*365*24*3600) // yea yea, I know
+            }
+            data.refresh {
+                lastUpdate = Date()
+           }
+        }
         res.send(json: computed)
         next()
     }
